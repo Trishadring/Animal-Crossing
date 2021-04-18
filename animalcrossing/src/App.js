624112ebf1e1dda90e1cfa27a5b4e0063f3afaa5
Trisha Dring <trisha.dring@gmail.com>
2021-04-18 11:44:17 -0400

    added state for hemisphere and further filtered fish by current hour

--- animalcrossing/src/App.js
+++ animalcrossing/src/App.js
@@ -1,52 +1,71 @@
 
 import './App.css';
 import { Route, Link, useContext} from "react-router-dom";
+import {Component} from 'react';
 import BugData from "./data/bugData";
 import FishData from "./data/fishData";
 import Bugs from "./components/bugs";
 import Fish from './components/fish';
 
 
-const App = () =>{
 
-	//const fish = FishData.fishList;
-	let today = new Date();
+class App extends Component {
+
+
+	initialState = () => ({
+    hemisphere: 'n',
+
+  });
+
+	state = this.initialState();
+
+	switchHemisphere = () => {
+    this.setState(prevState => ({
+			hemisphere: prevState.hemisphere === 'n' ? 's' : 'n'
+		}));
+  };
+
+	
+
+
+render() {
+	const today = new Date();
 	let currentHour = today.getHours();
-	let currentMonth = today.getMonth();
-	const fishMonthNorth = `n_${currentMonth}`;
-	const fishMonthSouth = `s_${currentMonth}`;
-	var Hour = `h_${currentHour}`;
-	let filterFish = FishData.fishList.filter(function (i) {
-		return i[fishMonthNorth] == true;
+	const currentMonth = today.getMonth();
+	const currentHemisphere = `${this.state.hemisphere}_${currentMonth}`;
+	const Hour = `h_${currentHour}`;
+	let filter = FishData.fishList.filter(function (i) {
+		return i[currentHemisphere] === true;
 	});
-
-console.log(filterFish);
-
-  return (
-    <div className="my_container">	
-			<div className="top">
-				<h1>Animal Crossing New Horizon </h1>
-				<nav>
-					<Link className="btn btn-outline-primary" to="/BugList">Bug List</Link>
-					&nbsp;
-					<Link className="btn btn-outline-primary" to="/FishList">Fish List</Link>
-					&nbsp;
-					<Link className="btn btn-outline-primary" to="/BothList">Bugs and Fish</Link>
-				</nav>
-				<Route  path="/BugList" render={() => <Bugs bugs={BugData.bugList} />}/>
-				<Route  path="/FishList" render={() => <Fish fish={filterFish} />}/>	
-				<Route  path="/BothList" render={() => {
-				<>
-				<h1>Fish</h1>
-				<Fish fish={filterFish} />
-				<h1>Bugs</h1>
-				{/* <Bugs bugs={BugData.bugList} /> */}
-				</>
-			}
-				}/>				
-			</div>		
-		</div>
-  );
+	let filterTime = filter.filter(function (i) {
+		return i[Hour] === true;
+	});
+		return (
+			<div className="my_container">	
+				<div className="top">
+					<h1>Animal Crossing New Horizon </h1>
+					<nav>
+						<Link className="btn btn-outline-primary" to="/BugList">Bug List</Link>
+						&nbsp;
+						<Link className="btn btn-outline-primary" to="/FishList">Fish List</Link>
+						&nbsp;
+						<Link className="btn btn-outline-primary" to="/BothList">Bugs and Fish</Link>
+					</nav>
+					<Route  path="/BugList" render={() => <Bugs bugs={BugData.bugList} />}/>
+					<Route  path="/FishList" render={() => <Fish fish={filterTime} />}/>	
+					<Route  path="/BothList" render={() => {
+					<>
+					<h1>Fish</h1>
+					<Fish fish={filterTime} />
+					<h1>Bugs</h1>
+					{/* <Bugs bugs={BugData.bugList} /> */}
+					</>
+				}
+					}/>				
+				</div>		
+			</div>
+		)
+	}
 }
 
 export default App;
